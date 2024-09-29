878c74306f3e1769ab3752588fa444dbe5a8e1a3
unknown <dieuveilparkermahoukou@gmail.com>
2024-09-29 15:29:44 +0200

    Atelier 2

---
+++ atelier2/Models/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace atelier2.Models.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Get(int Id);
+        List<T> Search(string term);
+        IEnumerable<T> GetAll();
+        T Add(T t);
+        T Update(T t);
+        T Delete(int Id);
+    }
+}
